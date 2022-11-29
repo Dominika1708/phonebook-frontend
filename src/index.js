4521959e134e2536905a8858ef3b488d8809b18a
Dominika Sosnowska <domi.sosnowska@o2.pl>
2022-11-29 20:03:24 +0100

    update

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
-    <React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
       <App />
-    </React.StrictMode>
-  </Provider>
+    </Provider>
+  </React.StrictMode>
 );
