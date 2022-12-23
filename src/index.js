eec2e446945e765ffb82c4af923b11cdb6a2438b
Dominika Sosnowska <domi.sosnowska@o2.pl>
2022-12-23 20:10:12 +0100

    pages

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter  basename="/goit-react-hw-08-phonebook">
         <App />
       </BrowserRouter>
     </Provider>
