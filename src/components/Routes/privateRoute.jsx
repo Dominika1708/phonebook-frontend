61f097a81a2e0c60527767a2e556cf46b1c8b49a
Dominika Sosnowska <domi.sosnowska@o2.pl>
2022-12-23 19:19:36 +0100

    update

--- src/components/Routes/privateRoute.jsx
+++ src/components/Routes/privateRoute.jsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { selectIsLoggedIn } from 'redux/auth/selectors';
 
-export const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
+export const PrivateRoute = ({ component: Component, redirectTo }) => {
   const isLoggedin = useSelector(selectIsLoggedIn);
   return isLoggedin ? Component : <Navigate to={redirectTo} />;
 };
--- src/components/Routes/restrictedRoute.jsx
+++ src/components/Routes/restrictedRoute.jsx
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 import { selectIsLoggedIn } from 'redux/auth/selectors';
 
-export const RestrictedRoute = ({ component: Component, redirectTo = '/' }) => {
+export const RestrictedRoute = ({ component: Component, redirectTo }) => {
   const isLoggedin = useSelector(selectIsLoggedIn);
   return isLoggedin ? <Navigate to={redirectTo} /> : Component;
 };
